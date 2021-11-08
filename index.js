746bde75c3237a32042be81852c3be6a594d227b
Caitlin Thompson <ctt00297@gmail.com>
2021-11-08 12:50:42 -0600

    completed

---
+++ index.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="description" content="best website ever" />
+        <meta name="keywords" content="f13, rocks, awesome" />
+
+        <title>F13 ROCKS NEWSLETTER</title>
+
+        <link rel="stylesheet" href="./index.css" />
+    </head>
+    <body>
+        <div>
+            <h1>F13 NEWSLETTER</h1>
+            <h2>F13 NEWSLETTER</h2>
+            <h3>F13 NEWSLETTER</h3>
+            <h4>F13 NEWSLETTER</h4>
+            <h5>F13 NEWSLETTER</h5>
+            <h6>F13 NEWSLETTER</h6>
+
+            <input type="text" placeholder="enter username"/>
+            <input type="password" placeholder="enter password" />
+            <button>submit</button>
+        </div>
+
+        <div>
+            <a href="https://www.google.com" target="_blank">Bing!</a>
+
+            <p>"Lorem ipsum dolor sit amet</p>
+
+            <ul>
+                <li>banana</li>
+                <li>apple</li>
+                <li>orange</li>
+            </ul>
+
+            <ol>
+                <li>banana</li>
+                <li>apple</li>
+                <li>orange</li> 
+            </ol>
+
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg"
+            alt="Cat" />
+        </div>
+    </body>
+</html>
